d02708562a08e6245c78be2df83e215e52882d9e
ignat14 <ignat14@gmail.com>
2026-03-07 13:37:23 +0100

    feat: add github PR  workflow

---
+++ app/config.py
@@ -0,0 +1,13 @@
+from enum import Enum
+from typing import Literal
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+    ENVIRONMENT: Literal["local", "production"] = "local"
+
+
+settings = Settings()
